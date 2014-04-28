9553df140290bffedbaa5bc83302801525599632
Anna Cho <annacho13@gmail.com>
2014-04-27 23:48:27 -0400

    got rid of non-essential files

--- db/migrate/20140428032700_fix_garage_items.rb
+++ db/migrate/20140428032700_fix_garage_items.rb
@@ -1,12 +1,14 @@
-class CreateGarageItems < ActiveRecord::Migration
+class FixGarageItems < ActiveRecord::Migration
   def change
-    create_table :garage_items do |t|
+  	drop_table "garage_items"
+
+  	create_table "garage_items", force: true do |t|
 	    t.string  "category"
 	    t.string  "headline"
 	    t.integer "price"
 	    t.string  "condition"
 	    t.string  "description"
 	    t.date    "submit_date"
-    end
+	  end
   end
 end
