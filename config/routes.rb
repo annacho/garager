13ec9059cad57606be73eee4ddaaee4f3158fd3a
Anna Cho <annacho13@gmail.com>
2014-04-29 08:14:39 -0400

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,6 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'welcome#index'
 
-  resources :items
-
   resources :garage_items
 
   # Example of regular route:
